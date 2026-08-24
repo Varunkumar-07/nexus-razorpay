defcdd1f4023b6806b56f197b277c248293a6f0a
Varunkumar-07 <varunkumar.u7@gmail.com>
2026-08-24 13:30:59 +0530

    Wire /stats route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import CatalogPage from "./pages/CatalogPage";
 import ChatPage from "./pages/ChatPage";
+import StatsPage from "./pages/StatsPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<ChatPage />} />
       <Route path="/products" element={<CatalogPage />} />
+      <Route path="/stats" element={<StatsPage />} />
     </Routes>
   );
 }
